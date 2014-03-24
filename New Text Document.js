52564123d97d01038e9df6aad88430dfc5011941
mercviper <mercviper@gmail.com>
2014-03-24 03:06:42 -0500

    Changed to before messing with targeting AI.

--- New Text Document.js
+++ New Text Document.js
@@ -3,6 +3,18 @@
 // this.warcry() gives allies within 10m 30% haste for 5s, every 10s.
 // this.terrify() sends foes within 30m fleeing for 5s, once per match.
 
+var friends = this.getFriends();
+var enemies = this.getEnemies();
+if (enemies.length === 0) return;  // Chill if all enemies are dead.
+var enemy = this.getNearest(enemies);
+var friend = this.getNearest(friends);
+if (typeof this.terrifyTimer === 'undefined')
+    this.terrifyTimer=0;
+
+// Which one do you do at any given time? Only the last called action happens.
+this.attack(enemy);
+var nearAnEnemy = this.distance(enemy) < 4;
+var nearAFriend = this.distance(friend) < 4;
 
 this.findType = function(units, type) {
     for (var i = 0; i < units.length; ++i) {
@@ -12,26 +24,18 @@ this.findType = function(units, type) {
     }
     return null;
 };
-var friends = this.getFriends();
-var enemies = this.getEnemies();
-if (enemies.length === 0) return;  // Chill if all enemies are dead.
-var enemy = this.getNearest(enemies);
-var friend = this.getNearest(friends);
-var archer=this.findType(enemies, 'thrower');
 var hero=this.findType(enemies, 'shaman');
 if(hero!==null){
-    this.say("Attack!", {target: hero});
+   // this.say('attack hero shaman');
+    for(var i=0; i<friends.length; i++){
+        if(friends[i].type==='archer'){
+           // friends[i].attack(hero);
+        }
+    }
 }
 else{
     hero=this.findType(enemies, 'brawler');
 }
-if (typeof this.terrifyTimer === 'undefined')
-    this.terrifyTimer=0;
-
-// Which one do you do at any given time? Only the last called action happens.
-this.attack(enemy);
-var nearAnEnemy = this.distance(enemy) < 4;
-var nearAFriend = this.distance(friend) < 4;
 
 if(nearAnEnemy) {
      this.shield();//this.attack(nearestEnemy);
@@ -51,22 +55,15 @@ if((this.now()-this.terrifyTimer)<5){
     }
 }
 
-
-if(archer!==null){
-    this.say("Attack!", {target: archer});
-}
-
-if(hero.id==='Ironjaw'){
-    this.attack(hero);
-}
-
-if(!this.getCooldown('terrify') && this.health<150){
+if(!this.getCooldown('terrify') && this.health<100){
     this.terrifyTimer=this.now();
     this.terrify();
 }
 
+
 // You can also command your troops with this.say():
 //this.say("Defend!", {targetPos: {x: 30, y: 30}}));
+//this.say("Attack!", {target: enemy});
 //this.say("Move!", {targetPos: {x: 40, y: 40});
 
 // You can store state on this across frames:
